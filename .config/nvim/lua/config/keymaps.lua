114a30dbe01f1fb7dd241411e42072a8f97d4e72
Maciej Caderek <maciej.caderek@gmail.com>
2023-11-14 21:34:19 +0100

    Improved Vifm theme and added Vifm it to Neovim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,6 +1,16 @@
+local Util = require("lazyvim.util")
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
 vim.keymap.set({ "i", "x", "n", "s" }, "<C-m>", "<cmd>DevdocsOpenFloat<cr>")
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+-- Add shortcuts to open TUI file manager in the built-in terminal
+vim.keymap.set("n", "<leader>fm", function()
+  Util.terminal({ "vifm", ".", "." }, { cwd = Util.root(), esc_esc = false, ctrl_hjkl = false })
+end, { desc = "File manager" })
+
+vim.keymap.set("n", "<leader>fM", function()
+  Util.terminal({ "vifm", "/home/caderek/", "." }, { cwd = Util.root(), esc_esc = false, ctrl_hjkl = false })
+end, { desc = "File manager (with home)" })
