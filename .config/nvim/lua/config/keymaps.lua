a44bd132c78537eb1bc427cff6f30b8d01d5d5b2
Maciej Caderek <maciej.caderek@gmail.com>
2023-11-17 20:27:09 +0100

    Added Harpoon to Neovim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,4 +1,7 @@
 local Util = require("lazyvim.util")
+local wk = require("which-key")
+local harpoonMark = require("harpoon.mark")
+local harpoonUI = require("harpoon.ui")
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
@@ -19,3 +22,24 @@ end, { desc = "File manager (home dir + cwd)" })
 vim.keymap.set("n", "<leader>m", function()
   Util.terminal({ "cmus" }, { cwd = Util.root(), esc_esc = true, ctrl_hjkl = false })
 end, { desc = "Music player" })
+
+-- Add shortcuts to open TUI typing test in the built-in terminal
+vim.keymap.set("n", "<leader>t", function()
+  Util.terminal({ "/home/caderek/.cargo/bin/toipe" }, { cwd = Util.root(), esc_esc = true, ctrl_hjkl = false })
+end, { desc = "Typing test" })
+
+-- Add Harpoon shourtcuts
+wk.register({
+  ["<leader>h"] = { name = "+harpoon" },
+})
+
+vim.keymap.set("n", "<leader>ha", harpoonMark.add_file, { desc = "Add" })
+vim.keymap.set("n", "<leader>hl", harpoonUI.toggle_quick_menu, { desc = "Quick menu" })
+vim.keymap.set("n", "<leader>h[", harpoonUI.nav_prev, { desc = "Prev" })
+vim.keymap.set("n", "<leader>h]", harpoonUI.nav_next, { desc = "Next" })
+
+for i = 1, 8 do
+  vim.keymap.set("n", "<leader>h" .. tostring(i), function()
+    harpoonUI.nav_file(i)
+  end, { desc = "Jump to file " .. tostring(i) })
+end
